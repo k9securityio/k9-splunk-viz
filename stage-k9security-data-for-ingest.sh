814d04fe7a714dffa0cbe76c3532cc4290029d36
Stephen Kuenzli <skuenzli@k9security.io>
2021-10-05 09:29:53 -0700

    Create basic shell script to stage recent k9security csv reports and load into Splunk input directory.

---
+++ stage-k9security-data-for-ingest.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+src_dir="${HOME}/tmp/k9security-inbox/reports/aws"
+pushd "$src_dir"
+
+stage_dir="${HOME}/tmp/splunk-inputs/k9security"
+
+src_files=$(find . -mtime -7d -name '*.csv' | sed 's|./||')
+splunk_input_dir='/var/local/splunk-inputs/k9security'
+
+echo "staging k9security files in ${stage_dir}"
+mkdir -p "${stage_dir}"
+for src_file in $src_files; do
+  dest_file=$(echo "$src_file" | sed 's|/|_|g')
+  echo "src_file: ${src_file} dest_file: ${dest_file}"
+  cp "${src_file}" "${stage_dir}/${dest_file}"
+done;
+
+popd
+
+echo "copying staged files to ${splunk_input_dir}"
+docker cp "${stage_dir}/" "splunk-integration_splunk_1:${splunk_input_dir}"
+echo "done ;)"
